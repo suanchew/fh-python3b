4142c3e2a7ee04e4be6ce7a30703ab17cd5809b8
suanchew <suanschew@gmail.com>
2022-10-03 02:26:28 +0000

    Add deckComplete

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 class Card:
-    def __init__(self, suit, valu):
-        self.suit = suit
+    def __init__(self, sui, valu):
+        self.suit = sui
         self.value = valu
 
     def show(self):
@@ -15,15 +15,30 @@ class Deck:
 
     def build52(self):
         for s in ["hearts", "diamonds", "clubs", "spades"]:
-            for v in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
+            for v in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+                "J", "Q", "K"]:
                 self.cards.append(Card(s, v))
  
     def show(self):
         for c in self.cards:
             c.show()
 
-    def dealCard(self):
-        self.cards.pop()
+    def deal(self):
+        cardDealt = self.cards.pop()
+        print ("Card dealt>>> ")
+        cardDealt.show()
+        print ("<<<")
+
+    def deckComplete(self):
+        selfCards = []
+        for c in self.cards:
+            selfCards.append(c.value + " " + c.suit)
+        for s in ["hearts", "diamonds", "clubs", "spades"]:
+            for v in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+                "J", "Q", "K"]:
+                if not (v + " " + s) in selfCards:
+                    return False
+        return True
 
 # Fisher-Yates shuffle algortihm
 # To shuffle an array a of n elements (indices 0..n-1):
@@ -31,9 +46,13 @@ class Deck:
 #         j = random integer with 0 <= j <= i
 #             exchange a[j] and a[i]
     def shuffle(self):
+        if not self.deckComplete():
+            return False
+        print ("deck is complete!!!yay")
         for i in range(len(self.cards)-1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+        return True
 
 print ("Name: Suan Chew")
 print ("Student ID: 20153703")
@@ -44,6 +63,5 @@ print ("Course policy example: It is a student’s responsibility to drop himsel
 
 myDeck = Deck()
 myDeck.shuffle()
+myDeck.deal()
 myDeck.show()
-
-
