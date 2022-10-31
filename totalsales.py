2992b063073eba26e4244d183d3f477b48a9ffe3
suanchew <suanschew@gmail.com>
2022-10-31 03:08:08 +0000

    Add notes

--- totalsales.py
+++ totalsales.py
@@ -85,9 +85,8 @@ statesCapitals={
     'wisconsin':'madison',
     'wyoming':'cheyenne'
 }
+
 guesses = Counter({'correct':0, 'incorrect':0})
-
-
 keepGuessing = True
 while keepGuessing:
     for k in random.sample(statesCapitals.keys(), 1):
@@ -108,26 +107,4 @@ while keepGuessing:
         keepGuessing = False
 
 print("You made {} correct, and {} incorrect guesses".format(guesses['correct'], guesses['incorrect']))
-
-
-#unicodedata.normalize("NFKD", text.casefold())
-
-
-
-# > git pull --tags origin main
-# From https://github.com/suanchew/fh-python3b
-#  * branch            main       -> FETCH_HEAD
-# hint: You have divergent branches and need to specify how to reconcile them.
-# hint: You can do so by running one of the following commands sometime before
-# hint: your next pull:
-# hint: 
-# hint:   git config pull.rebase false  # merge
-# hint:   git config pull.rebase true   # rebase
-# hint:   git config pull.ff only       # fast-forward only
-# hint: 
-# hint: You can replace "git config" with "git config --global" to set a default
-# hint: preference for all repositories. You can also pass --rebase, --no-rebase,
-# hint: or --ff-only on the command line to override the configured default per
-# hint: invocation.
-# fatal: Need to specify how to reconcile divergent branches.
 
