c309e2e8a2a86df2d5be8e8b1fac1f9ee3422e57
suanchew <suanschew@gmail.com>
2022-11-14 18:34:40 +0000

    Add notes on map, listcomp

--- bookorders.py
+++ bookorders.py
@@ -66,3 +66,64 @@ def modifySpell(spell):
 print( list(starmap(lambda n, m: modifySpell(n) * m, zip(spells, occurrence)) ) )
 
 
+
+# MAP FUNCTION:
+
+# The map function has two arguments - a function and an iterable.
+# Applies the function to each element of the iterable and returns a map object.
+# The function can be a normal, built-in or anonymous function.
+# List, sets, tuples, and other iterables can be used as the second argument of the map function.
+# The returned map object can be easily converted into another iterable using built-in functions.
+
+# Map functions as methods or built-in functions.
+#     cities = ['madrid', 'munich', 'valencia']
+#     cities_cap = list(map(lambda x:x.title(),cities))
+#     #cities_cap = ['Madrid', 'Munich', 'Valencia']
+#     elements = [1,2,3,4]
+#     elements_str = list(map(str,elements))
+#     #elements_str = ['1','2','3','4']
+
+# Iterable as a dictionary:
+#     elements = {'hidrogen':1, 'helium':2, 'carbon':6}
+#     elements_upper = list(map(lambda x:x.title(),elements))
+#     #elements_upper = ['Hidrogen','Helium','Carbon']
+
+# Iterable as a list of tuples:
+#     students = [('Amanda','161cm','51kg'), ('Patricia','165cm','61kg'), ('Marcos','191cm', '101kg')]
+#     students_height = list(map(lambda x:x[1], students))
+#     #list of strings
+#     #students_height = ['161cm','165cm','191cm']
+#     students_weight = list(map(lambda x:int(x[2][:-2]), students))
+#     #list of numbers
+#     #students_weight = [51, 61, 101]
+
+# The map function returns a map object which is an iterator. The iterator can be easily transformed into a list, set, or tuple:
+#     elements = [1,2,3,4]
+#     elements_by2 = map(lambda x:x*2,elements)
+#     print(type(elements_by2))
+#     #<class 'map'>
+
+#     elements_list = list(map(lambda x:x*2,elements))
+#     print(type(elements_list))
+#     #<class 'list'>
+#     elements_set = set(map(lambda x:x*2,elements))
+#     print(type(elements_set))
+#     #<class 'set'>
+#     elements_tuple = tuple(map(lambda x:x*2,elements))
+#     print(type(elements_tuple))
+#     #<class 'tuple'>
+#https://levelup.gitconnected.com/using-the-map-function-in-python-f088fad8788d
+
+
+# LIST COMPREHENSION - GENERATOR VS ITERATOR:
+
+# List comprehensions must be enclosed by square brackets [...]:
+#     h = [x for x in results if x.get("ID")=="91149"]
+# Using parenthesis (...) will create a generator expression:
+#     h = (x for x in results if x.get("ID")=="91149")
+# It is inefficient to read a whole list into memory when all you want is the first item that meets a condition. It is usually faster to use next and a generator expression:
+#     h = next(x for x in results if x.get("ID")=="91149")
+# Unlike list comprehension (which does it all at once), this solution will yield the items one at a time. It will stop once it finds an item that meets the condition. However, it will also raise a StopIteration error if it cannot find the item. To avoid this, give next a default value to return:
+#     h = next((x for x in results if x.get("ID")=="91149"), None)
+#https://stackoverflow.com/questions/21790459/bad-syntax-in-python-list-comprehension
+
