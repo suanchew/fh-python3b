280c6e25660c9fbb90d203da0d518cdab6e67a52
suanchew <suanschew@gmail.com>
2022-11-14 18:46:31 +0000

    Edit totalsales py

--- totalsales.py
+++ totalsales.py
@@ -1,10 +1,10 @@
 
-# # Total Sales
+# Challenge 1 - Total Sales
 
-# # Design a program that asks the user to enter a store’s sales for each day of the week. The amounts should be stored in a list. Use a loop to calculate the total sales for the week and display the result.
+# Design a program that asks the user to enter a store’s sales for each day of the week. The amounts should be stored in a list. Use a loop to calculate the total sales for the week and display the result.
 
+# Store sales in dictionary
 # salesOfWeek = {'Monday':0, 'Tuesday':0, 'Wednesday':0, 'Thursday':0, 'Friday':0, 'Saturday':0, 'Sunday':0}
-
 # for day in salesOfWeek.keys():
 #     dailySale = float(input("Enter sale for {}: ".format(day)))
 #     print("Entered ${:,.2f}".format(dailySale))
@@ -14,18 +14,20 @@
 # sumWeekSales = sum(list(salesOfWeek.values()))
 # print("Total sales this week: ${:,.2f}".format(sumWeekSales))
 
-# listSales = []
-# weeklySales = 0
-# for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
-#     dailySale = float(input("Enter sale for {}: ".format(day)))
-#     print("Entered ${:,.2f}".format(dailySale))
-#     listSales.append(dailySale)
-# for amount in listSales:
-#     weeklySales += amount
-# print("Total sales this week = ${:,.2f}".format(weeklySales))
+# Store sales in list
+listSales = []
+weeklySales = 0
+for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
+    dailySale = float(input("Enter sale for {}: ".format(day)))
+    print("Entered ${:,.2f}".format(dailySale))
+    listSales.append(dailySale)
+for amount in listSales:
+    weeklySales += amount
+print("Total sales this week = ${:,.2f}".format(weeklySales))
 
 
-# Capital Quiz
+
+# Challenge 2 - Capital Quiz
 
 # Write a program that creates a dictionary containing the U.S. states as keys, and their capitals as values. (Use the Internet to get a list of the states and their capitals.) The program should then randomly quiz the user by displaying the name of a state and asking the user to enter that state’s capital. The program should keep a count of the number of correct and incorrect responses. (As an alternative to the U.S. states, the program can use the names of countries and their capitals.)
 
