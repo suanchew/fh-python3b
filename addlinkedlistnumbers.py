6d736acaac436565d64716b6755e7d8e5cb01327
suanchew <suanschew@gmail.com>
2022-12-12 06:25:47 +0000

    Add Challenge 1 question

--- addlinkedlistnumbers.py
+++ addlinkedlistnumbers.py
@@ -1,6 +1,14 @@
 
 import linkedlist_singly as ll
 
+# Challenge 1
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contains a single digit. Add the two numbers and return them as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself. For example,
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807
+
 def add_two_numbers(l1: ll.LinkedList, l2: ll.LinkedList) -> ll.LinkedList:
 
     l3 = ll.LinkedList()
